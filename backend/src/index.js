4ea08a5ae97338c2a8f0d6cbec4d9a6560e90e7c
Tiago Carnevalli Elias <TiagoCarnevalli@users.noreply.github.com>
2020-03-26 16:40:18 -0300

    Back-end da Aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,54 @@
+const express = require ('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * npm: Instala um pacote
+ * npx: Executa um pacote
+ */
+
+/**
+ * Rota / Recurso
+ * 
+ * Rota para o recurso de usuários 'localhost:3333/users'
+ * 
+ * app.get('/users', (request, response) => {});
+ */
+
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar/Listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de Parâmetros
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação...)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * Bancos de Dados
+ * 
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
+
+app.listen(3333);
