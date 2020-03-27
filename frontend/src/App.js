d56a0afda4473d8b7aa6d1368470f501722f8f69
Tiago Carnevalli Elias <TiagoCarnevalli@users.noreply.github.com>
2020-03-27 16:17:46 -0300

    Atualização

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
-import React, {useState} from 'react';
-import Header from './Header'
+import React from 'react';
+import './global.css';
+import Logon from './pages/Logon';
 
 // JSX (JavaScript & XML)
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  // Array [valor, funcaoDeAtualizacao]
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    //<Header title="Semana Omnistack" />
-    <div>
-      <Header> Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
 }
 
